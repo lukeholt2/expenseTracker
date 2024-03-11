86db55d3f42aa6dd0b9a3bd8124db1ca8cec293c
Luke Holt <lukeholt2@protonmail.com>
2024-03-10 21:27:40 -0400

    adding some hooks to setup more filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,34 +4,48 @@ import { Expense } from '../models/expense';
 import { ModalController } from '@ionic/angular';
 import { NewExpenseDialogComponent } from '../new-expense-dialog/new-expense-dialog.component';
 
+
+interface ListFilters {
+  month: number;
+  year: number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit {
 
-  constructor(public expenseService: ExpenseService, private modalCtrl: ModalController) { }
+  constructor(public expenseService: ExpenseService, private modalCtrl: ModalController) {
+    const today = new Date();
+    this.expenseFilter = {
+      month: today.getMonth(),
+      year: today.getMonth()
+    }
+  }
 
   total: number = 0;
+  listFilter: string = 'all'
+
+  expenseFilter: ListFilters;
 
   ngOnInit() {
-    const today = new Date();
-    this.expenseService.getAll(today.getDate(), today.getMonth())
+    this.expenseService.getAll(this.expenseFilter.month, this.expenseFilter.year)
       .subscribe((data: Expense[]) => {
         this.total = 0;
         data.forEach((expense: Expense) => this.total += expense.amount);
       })
   }
 
-  async onAdd(){
+  async onAdd() {
     const modal = await this.modalCtrl.create({
       component: NewExpenseDialogComponent,
     });
     modal.present();
 
     const { data, role } = await modal.onWillDismiss();
-    if(role == 'confirm'){
+    if (role == 'confirm') {
       this.expenseService.addExpense(data).subscribe((data) => console.log(data));
     }
   }
