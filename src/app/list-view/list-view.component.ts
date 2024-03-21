b8b0bf5f7e668d90baa6ba4240cbd6488a055118
Luke Holt <lukeholt2@protonmail.com>
2024-03-21 19:57:06 -0400

    adds scroll spinner

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Expense } from '../models/expense';
-import { IonContent, IonLabel, IonList, IonItem } from '@ionic/angular/standalone'
+import { IonContent, IonLabel, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/angular/standalone'
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss'],
-  imports: [CommonModule, IonContent, IonLabel, IonList, IonItem],
+  imports: [CommonModule, IonContent, IonLabel, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent],
   standalone: true
 })
 export class ListViewComponent  implements OnInit {
