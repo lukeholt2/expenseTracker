5d0769864303f75cb9029d588a614225bb99446b
Luke Holt <lukeholt2@protonmail.com>
2024-10-05 06:53:22 -0400

    adding virtual scroll + some cleanup

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,8 +1,9 @@
-import { Component, ComponentRef, Input, OnInit, model } from '@angular/core';
+import { Component, ComponentRef, Input, OnInit, model, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Expense } from '../models/expense';
 import { ModalController } from '@ionic/angular';
-import { IonContent, IonLabel, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonGrid, IonRow, IonCol  } from '@ionic/angular/standalone'
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { IonContent, IonLabel, IonList, IonItem, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 import { CommonModule } from '@angular/common';
 import { NewExpenseDialogComponent } from '../new-expense-dialog/new-expense-dialog.component';
 import { ExpenseService } from '../services/expense.service';
@@ -11,39 +12,19 @@ import { ExpenseService } from '../services/expense.service';
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss'],
-  imports: [CommonModule, IonContent, IonLabel, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonGrid, IonRow, IonCol],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, IonContent, ScrollingModule,
+    IonLabel, IonList, IonItem,
+    IonGrid, IonRow, IonCol],
   standalone: true
 })
-export class ListViewComponent  implements OnInit {
+export class ListViewComponent {
 
+  @Input() list!: Observable<Expense[]>;
 
-  @Input() updateList!: Observable<Expense[]>;
-  
-  list: Expense[] = [];
-  
-  constructor(public expenseService: ExpenseService, private modalCtrl: ModalController) { }
+  @Output() onItemSelected: EventEmitter<Expense> = new EventEmitter<Expense>();
 
-  ngOnInit() {
-    this.updateList.subscribe((data) => this.list = data);
-  }
-
-  async onView(expense: Expense) {
-    const modal = await this.modalCtrl.create({
-      component: NewExpenseDialogComponent,
-      componentProps: {
-        expense: expense
-      }
-    });
-    
-    modal.present();
-
-    const { data, role } = await modal.onWillDismiss();
-    if (role == 'confirm') {
-      console.log(data);
-      this.expenseService.editExpense(data)
-        .subscribe((data) => {
-            this.updateList.subscribe((data) => this.list = data);
-        });
-    }
+  onView(expense: Expense) {
+     this.onItemSelected.emit(expense)
   }
 }
