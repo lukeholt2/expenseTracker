751f88e89cef11bd3a6b358f604e478568c24a63
Luke Holt <lukeholt2@protonmail.com>
2024-10-05 07:11:52 -0400

    fixing list renders between filter changes

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,18 +1,14 @@
-import { Component, ComponentRef, Input, OnInit, model, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Expense } from '../models/expense';
-import { ModalController } from '@ionic/angular';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { IonContent, IonLabel, IonList, IonItem, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 import { CommonModule } from '@angular/common';
-import { NewExpenseDialogComponent } from '../new-expense-dialog/new-expense-dialog.component';
-import { ExpenseService } from '../services/expense.service';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, IonContent, ScrollingModule,
     IonLabel, IonList, IonItem,
     IonGrid, IonRow, IonCol],
