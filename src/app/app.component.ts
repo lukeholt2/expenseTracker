4a006a2e9ed603afe87f8e3122f9104b810d6a72
Luke Holt <lukeholt2@protonmail.com>
2024-11-29 15:10:26 -0500

    converting menu to tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet,
-  IonTitle,  IonItem, IonLabel, IonList,
-  IonHeader, IonContent,
+  IonTitle,  IonItem, IonLabel, IonList, IonTabButton,
+  IonHeader, IonContent, IonTabBar, IonTabs, IonIcon,
   IonMenu, IonToolbar, IonMenuButton} from '@ionic/angular/standalone'
 import { AuthenticationService } from './services/authentication.service';
+import { addIcons } from 'ionicons';
+import { walletOutline, logOutOutline, cashOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [IonApp, IonRouterOutlet, IonTitle,
-    IonItem, IonLabel, IonList, IonHeader, IonContent,
+  imports: [CommonModule, IonApp, IonRouterOutlet, IonTitle, IonTabBar, IonTabs, IonTabButton,
+    IonItem, IonLabel, IonList, IonHeader, IonContent, IonIcon,
     IonMenu, IonToolbar, IonMenuButton],
   standalone: true
 })
 export class AppComponent {
-  constructor(private authenticationService: AuthenticationService,
-              private router: Router) {}
+  constructor(public authenticationService: AuthenticationService,
+              private router: Router) {
+                addIcons({ walletOutline, logOutOutline, cashOutline });
+              }
 
   logout(){
     this.authenticationService.logout();
-    this.router.navigate(['/login'])
   }
 
   navigate(route: string){
